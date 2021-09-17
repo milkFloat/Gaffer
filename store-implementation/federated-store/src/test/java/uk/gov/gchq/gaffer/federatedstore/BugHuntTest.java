fe04eba02f4d40323076cd10718b5c8af4308b11
t92549 <80890692+t92549@users.noreply.github.com>
2021-09-17 15:44:35 +0100

    gh-2421: More fixes for bug hunt

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/BugHuntTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/BugHuntTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gov.gchq.gaffer.federatedstore;
 
 import org.junit.Assert;
@@ -28,7 +43,6 @@ import uk.gov.gchq.gaffer.store.schema.SchemaEdgeDefinition;
 import uk.gov.gchq.gaffer.types.TypeSubTypeValue;
 import uk.gov.gchq.gaffer.user.User;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -249,7 +263,6 @@ public class BugHuntTest {
 
         Assert.assertEquals(1, list.size());
         Assert.assertEquals(EDGE, list.get(0));
-        // FAIL: Json result lacks a "matchedVertex"
         Assert.assertEquals(EXPECTED, new String(JSONSerialiser.serialise(list.get(0), true)));
     }
 
